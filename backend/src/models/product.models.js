76819f9a379c418e22760430ce1c3d30c1d6a0fb
HaseethVardhan <haseethvardhan01@gmail.com>
2025-02-14 13:49:39 +0530

    add product model with schema definition

---
+++ backend/src/models/product.models.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    productType: {
+        type: String,
+        enum: ['can', 'bottle', 'packet'],
+        required: true
+    },
+    supplierId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Supplier',
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    cost: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    minimumOrderQuantity: {
+        type: Number,
+        required: true,
+        min: 1
+    }
+}, {
+    timestamps: true
+});
+
+export const Product = mongoose.model('Product', productSchema);

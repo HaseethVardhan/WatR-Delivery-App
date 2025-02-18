de3b274235d66cccde5527e836ca9449b9d2ef59
HaseethVardhan <haseethvardhan01@gmail.com>
2025-02-18 13:11:50 +0530

    add OrderDetailsMin component and integrate it into SupplierHome page

---
+++ frontend/src/customComponents/OrderDetailsMin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OrderDetailsMin = () => {
+  return (
+    <div className='flex flex-col w-[95%] border-2 border-gray-100 rounded-lg p-2'>
+        <p className='font-semibold'>20 Water Can</p>
+        <p className=''>R K Beach, Visakhapatnam, Andhra Pradesh</p>
+    </div>
+  )
+}
+
+export default OrderDetailsMin
--- frontend/src/pages/SupplierHome.jsx
+++ frontend/src/pages/SupplierHome.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from '../customComponents/Navbar.jsx'
+import OrderDetailsMin from '../customComponents/OrderDetailsMin.jsx'
 
 const SupplierHome = () => {
   return (
-    <div>SupplierHome</div>
+    <div>
+      <Navbar />
+      <h1 className='font-bold text-lg p-3'>Today's Deliveries</h1>
+      <div className='w-screen flex justify-center'>
+        < OrderDetailsMin />
+      </div>
+    </div>
   )
 }
 

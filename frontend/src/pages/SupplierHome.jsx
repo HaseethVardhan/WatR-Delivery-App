d73a8d590e69afd63d8dcd29c0a8453cda02fa5f
HaseethVardhan <haseethvardhan01@gmail.com>
2025-02-20 21:37:20 +0530

    add navigation buttons for creating and viewing subscription models in SupplierHome

--- frontend/src/pages/SupplierHome.jsx
+++ frontend/src/pages/SupplierHome.jsx
@@ -1,32 +1,38 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../customComponents/Navbar.jsx";
 import OrderDetailsMin from "../customComponents/OrderDetailsMin.jsx";
-import axios from 'axios'
+import axios from "axios";
+import { Button } from "@/components/ui/button.jsx";
 
 const SupplierHome = () => {
+  const navigate = useNavigate();
+
   const [subscriptions, setSubscriptions] = React.useState();
 
-const [loading, setLoading] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
     const fetchSubscriptions = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
-        const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/supplier/get-subscriptions`, {
-          headers: {
-            Authorization : `Bearer ${localStorage.getItem("token")}`
+        const response = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/supplier/get-subscriptions`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
           }
-        });
+        );
 
         setSubscriptions(response.data.data.subscriptions);
       } catch (error) {
         console.error("Error fetching subscriptions:", error);
       }
-      setLoading(false)
+      setLoading(false);
     };
 
     fetchSubscriptions();
-
   }, []);
   return (
     <div>
@@ -36,14 +42,33 @@ const [loading, setLoading] = React.useState(false);
         </div>
       )}
       <Navbar />
+      <div className="flex flex-col max-w-[500px] w-[100%] justify-self-center p-3 gap-2">
+        <Button
+          onClick={() => {
+            navigate("/supplier-create-subscription-model");
+          }}
+          className="bg-green-500 shadow-none"
+        >
+          New Subscription Model
+        </Button>
+        <Button
+          onClick={() => {
+            navigate("/supplier-view-subscription-model");
+          }}
+          className="shadow-none"
+          variant="secondary"
+        >
+          View All Subscription Models
+        </Button>
+      </div>
       <h1 className="font-bold text-lg p-3">Today's Deliveries</h1>
-      <div className="flex flex-col justify-center p-3">
-
-        {subscriptions!== undefined  && subscriptions.map((subscription) => (
-          <div key={subscription._id}>
-            <OrderDetailsMin subscription={subscription} />
-          </div>
-        ))}
+      <div className="flex flex-col justify-center p-3 ">
+        {subscriptions !== undefined &&
+          subscriptions.map((subscription) => (
+            <div key={subscription._id}>
+              <OrderDetailsMin subscription={subscription} />
+            </div>
+          ))}
       </div>
     </div>
   );
